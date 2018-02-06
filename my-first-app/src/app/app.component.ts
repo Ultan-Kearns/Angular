666b262a10c74bd46e7527ea722c06361fa13692
Ultan-Kearns <ultankearns@gmail.com>
2018-02-06 17:20:03 +0000

    Completed lab

---
+++ my-first-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  myName = "Ultan";
+  myAge = 22;
+}
